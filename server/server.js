d6e4304294ad94c35d610f58f8a5be85be41daae
carter cromer <cartergcromer@gmail.com>
2020-09-24 17:08:01 -0700

    Initial Push

---
+++ server/server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var logger = require('morgan');
+var path = require('path');
+var methodOverride = require('method-override')
+var cors = require('cors');
+
+var app = express();
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(methodOverride());
+app.use(cors());
+app.use('/public', express.static('public'));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+app.listen(process.env.PORT || 8080);
