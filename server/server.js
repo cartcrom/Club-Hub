b3176f678456f1ea37287bf634669ec0337b3d81
Jayant Devkar <55962509+JayantDevkar@users.noreply.github.com>
2020-09-25 09:32:45 +0530

    backend
    backend setup

--- server/server.js
+++ server/server.js
@@ -1,19 +1,19 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var logger = require('morgan');
-var path = require('path');
-var methodOverride = require('method-override')
-var cors = require('cors');
+var express = require("express");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var path = require("path");
+var methodOverride = require("method-override");
+var cors = require("cors");
 
 var app = express();
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(methodOverride());
 app.use(cors());
-app.use('/public', express.static('public'));
+app.use("/public", express.static("public"));
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "/public/index.html"));
+  res.send("sup?");
 });
 
 app.listen(process.env.PORT || 8080);
