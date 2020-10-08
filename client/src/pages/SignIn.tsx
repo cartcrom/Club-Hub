f763afefe6f6dcd4f98bd5b4e5a7acc57ebb851e
Vance Winstead <vance.winstead@gmail.com>
2020-10-08 00:04:09 -0700

    Finished button styling and for sign up page

--- client/src/pages/SignIn.tsx
+++ client/src/pages/SignIn.tsx
@@ -1,18 +1,41 @@
 import React from 'react';
-import { IonContent, IonChip, IonLabel, IonPage, IonGrid } from '@ionic/react';
+import { IonContent, IonChip, IonLabel, IonPage, IonFooter, IonToolbar, IonTitle, IonHeader, IonButtons, IonBackButton, IonInput, IonIcon, IonItem} from '@ionic/react';
+import { chevronBackOutline, lockClosedOutline, personOutline } from 'ionicons/icons';
 import './FrontPage.css';
 import logo from '../images/CHLogo.png'
 
 const SignIn: React.FC = () => {
     return (
         <IonPage>
-            <IonContent>
-                <img src={logo} alt="ClubHub Logo" id="logo" className="center"/>
-                <span id="name" className="center">ClubHub</span>
-                <IonChip class="center">
-                    <IonLabel>Login</IonLabel>
-                </IonChip>
+            <IonHeader>
+                <IonToolbar>
+                    <IonButtons slot="start">
+                        <IonBackButton text=""/>
+                    </IonButtons>
+                </IonToolbar>
+            </IonHeader>
+
+            <IonContent fullscreen>
+                <img src={logo} alt="ClubHub Logo" id="logo"/>
+                <p id="name">ClubHub</p>
             </IonContent>
+
+            <IonFooter>
+                <IonItem className="chip-input">
+                    <IonIcon src={personOutline} slot="start"></IonIcon>
+                    <IonInput placeholder="email address"></IonInput>
+                </IonItem>
+                <IonItem className="chip-input">
+                    <IonIcon src={lockClosedOutline} slot="start"></IonIcon>
+                    <IonInput placeholder="password"></IonInput>
+                </IonItem>
+
+                <hr />
+
+                <IonItem className="chip-button">
+                    <IonLabel>Login</IonLabel>
+                </IonItem>
+            </IonFooter>
         </IonPage>
     );
 };
