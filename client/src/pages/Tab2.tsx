41a5febff7e0570cdea2f70f2ad186d27a4c514e
lukeUnderwood <luke.underwood42@gmail.com>
2020-10-21 11:20:32 -0700

    minor css changes

--- client/src/pages/Tab2.tsx
+++ client/src/pages/Tab2.tsx
@@ -1,19 +1,67 @@
-import React from 'react';
-import { IonButton, IonButtons, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import React, { useState } from 'react';
+import { IonButton, IonButtons, IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonCol, IonContent, IonGrid, IonHeader, IonImg, IonInput, IonItem, IonItemDivider, IonList, IonPage, IonRow, IonSearchbar, IonText, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab2.css';
 import { RouteComponentProps } from 'react-router';
 
+import club from '../images/ClubSoda.jpg'
+
 const Tab2: React.FC<RouteComponentProps> = (props) => {
+  
+  const [text, setText] = useState<string>();
+
   return (
     <IonPage>
       <IonHeader>
-        <IonToolbar>
+        <IonToolbar className="header">
           <IonTitle>Explore</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <ExploreContainer name="Tab 2 page" />
+      <IonContent>
+        <IonItemDivider>
+          <IonSearchbar value={text} placeholder="Search Clubs" onIonChange={e => setText(e.detail.value!)}></IonSearchbar>
+        </IonItemDivider>
+          <IonGrid>
+            <IonRow>
+              <IonCol>
+                <IonItem  className="tag" detail={false}>Recreational</IonItem>
+              </IonCol>
+              <IonCol>
+                <IonItem  button className="tag" detail={false}>Food</IonItem>
+              </IonCol>
+              <IonCol>
+                <IonItem  button className="tag" detail={false}>Poppin'</IonItem>
+              </IonCol>
+            </IonRow>
+          </IonGrid>
+        <IonText>Recommended</IonText>
+        <IonGrid>
+          <IonCol>
+            <IonCard button>
+              <IonImg src={club}></IonImg>
+              <IonCardTitle>Club Soda</IonCardTitle>
+              <IonGrid>
+                <IonRow>
+                  <IonCol>
+                    <IonItem color="medium">Recreational</IonItem>
+                  </IonCol>
+                  <IonCol>
+                    <IonItem color="medium">Food</IonItem>
+                  </IonCol>
+                  <IonCol>
+                    <IonItem color="medium">Poppin'</IonItem>
+                  </IonCol>
+                </IonRow>
+              </IonGrid>
+            </IonCard>
+          </IonCol>
+          <IonCol>
+            <IonCard button>
+              <IonImg src = "dunno"></IonImg>
+              <IonCardTitle>Club Name</IonCardTitle>
+            </IonCard>
+          </IonCol>
+        </IonGrid>
       </IonContent>
     </IonPage>
   );
