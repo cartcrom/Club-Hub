a28c179242a20f7bab1160ed5c4d4bb4b3b6871b
Vance Winstead <vance.winstead@gmail.com>
2020-11-01 23:09:19 -0800

    Account Info is stroed and printed to console; .edu email address is required

--- client/src/pages/front-pages/SignUp.tsx
+++ client/src/pages/front-pages/SignUp.tsx
@@ -1,15 +1,33 @@
 import React from 'react';
-import { IonContent, IonLabel, IonPage, IonFooter, IonToolbar, IonHeader, IonButtons, IonBackButton, IonInput, IonIcon, IonItem} from '@ionic/react';
-import { checkmarkCircleOutline, lockClosedOutline, personOutline, schoolOutline } from 'ionicons/icons';
+import { IonContent, IonLabel, IonPage, IonFooter, IonToolbar, IonHeader, IonButtons, IonBackButton, IonInput, IonIcon, IonItem, IonButton, IonChip} from '@ionic/react';
+import { alertCircleOutline, lockClosedOutline, mailOutline, personOutline} from 'ionicons/icons';
 import './FrontPage.css';
 import logo from '../../images/CHLogo.png'
 import { RouteComponentProps } from 'react-router';
+import { Controller, useForm } from 'react-hook-form';
+import { ErrorMessage } from '@hookform/error-message';
 
 interface LoginProps extends RouteComponentProps {
     setLogin: Function
 }
 
+type SignUpInfo = {
+    name: string;
+    college: string | undefined;
+    email: string;
+    password: string;
+}
+
 const SignUp: React.FC<LoginProps> = (props) =>{
+
+    const {control, handleSubmit, errors} = useForm<SignUpInfo>();
+
+    const registerUser = (data: SignUpInfo) => {
+        data.college = getCollege(data.email);
+        console.log(data); /* TODO: Send data to backend */
+        props.setLogin(true);
+    }
+
     return (
         <IonPage>
             <IonHeader>
@@ -20,34 +38,79 @@ const SignUp: React.FC<LoginProps> = (props) =>{
                 </IonToolbar>
             </IonHeader>
 
-            <IonContent fullscreen className="gradient-content">
+            <IonContent fullscreen className="gradient-content" scrollY={false}>
                 <img src={logo} alt="ClubHub Logo" id="logo"/>
                 <p id="name">ClubHub</p>
             </IonContent>
 
             <IonFooter className="transparent">
-                <IonItem className="chip-input">
-                    <IonIcon src={personOutline} slot="start"></IonIcon>
-                    <IonInput placeholder="email address"></IonInput>
-                </IonItem>
-                <IonItem className="chip-input">
-                    <IonIcon src={lockClosedOutline} slot="start"></IonIcon>
-                    <IonInput placeholder="password"></IonInput>
-                </IonItem>
-                <IonItem className="chip-input">
-                    <IonIcon src={checkmarkCircleOutline} slot="start"></IonIcon>
-                    <IonInput placeholder="retype password"></IonInput>
-                </IonItem>
-                <IonItem className="chip-input">
-                    <IonIcon src={schoolOutline} slot="start"></IonIcon>
-                    <IonInput placeholder="School"></IonInput>
-                </IonItem>
-
-                <IonItem button detail={false} className="chip-button bottom-button" onClick={() => {props.setLogin(true);}}>
-                    <IonLabel>Create New Acount</IonLabel>
-                </IonItem>
+                <form onSubmit={handleSubmit(registerUser)} id="form">
+                    <IonItem className="chip-input">
+                        <IonIcon src={personOutline} slot="start" />
+                        <Controller
+                            name="name"
+                            control={control}
+                            render={({onChange, onBlur}) => (
+                                <IonInput placeholder="full name" type="text" required
+                                    onIonChange={onChange} onIonBlur={onBlur} />
+                            )}
+                        />
+                    </IonItem>
+                    <IonItem className="chip-input">
+                        <IonIcon src={mailOutline} slot="start" />
+                        <Controller
+                            name="email"
+                            control={control}
+                            rules={{
+                                pattern: {
+                                    value: /.*edu/,
+                                    message: 'Email must end with .edu'
+                                }
+                            }}
+                            render= {({onChange, onBlur}) => (
+                                <IonInput placeholder="school email" type="email" required
+                                    onIonChange={onChange} onIonBlur={onBlur}/>
+                            )}
+                        />
+                    </IonItem>
+                    <ErrorMessage
+                        errors={errors}
+                        name="email"
+                        render = { ({message}) =>
+                            <div className="form-error">
+                                <IonIcon src={alertCircleOutline} color="danger"/>
+                                <IonLabel color="danger">{message}</IonLabel>
+                            </div>
+                        }
+                    />
+                    <IonItem className="chip-input">
+                        <IonIcon src={lockClosedOutline} slot="start" />
+                        <Controller
+                            name="password"
+                            control={control}
+                            render= {({onChange, onBlur}) => (
+                                <IonInput placeholder="password" type="password" enterkeyhint="done" required
+                                    onIonChange={onChange} onIonBlur={onBlur}/>
+                            )}
+                        />
+                    </IonItem>
+                    <IonButton expand="block" type="submit" className="chip-button" id="form-button">
+                        Create New Account
+                    </IonButton>
+                </form>
             </IonFooter>
         </IonPage>
     );
 };
 export default SignUp
+
+let getCollege = (email : string): string | undefined => {
+    let collegeEmails = new Map([
+        ['calpoly', "California Polytechnic State University, San Luis Obispo"]
+    ])
+    let start = email.indexOf('@') + 1;
+    let end = email.indexOf('.edu');
+    let collegeFromEmail = email.slice(start, end);
+    let college = collegeEmails.get(collegeFromEmail) ? collegeEmails.get(collegeFromEmail) : collegeFromEmail
+    return college
+}
