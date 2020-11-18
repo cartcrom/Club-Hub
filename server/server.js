dfd55a42d60162bca51125c5785cef49f0feb23e
Vance Winstead <vance.winstead@gmail.com>
2020-11-18 10:09:42 -0800

    Connected Login and Signup to frontend

--- server/server.js
+++ server/server.js
@@ -26,7 +26,7 @@ app.use(methodOverride("_method"));
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: ["http://localhost:3000"], //frontend server localhost:8080
+    origin: ["http://localhost:8100"], //frontend server localhost:8080
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true, // enable set cookie
   })
@@ -48,7 +48,7 @@ app.use(
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Credentials", true);
   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Origin", "http://localhost:8100");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-   Type, Accept, Authorization"
@@ -62,7 +62,8 @@ app.use(express.json());
 //Routes ##############################################################################
 app.post("/login", (req, res) => {
   console.log("*Login route called with this req:*", req.body)
-  auth.login(req.body.email, req.body.password).then((user) => {
+  auth.login(req.body.email, req.body.password)
+  .then((user) => {
     if (user) {
       req.session.user = user;
       helper.object_trimmer(user, ["password"]).then((usr) => {
@@ -70,6 +71,10 @@ app.post("/login", (req, res) => {
         res.send(usr)
       })
     }
+  })
+  .catch((err) => {
+    console.log(err)
+    res.status(401).send(err)
   })
 });
 
@@ -80,9 +85,19 @@ app.post("/verify/user", (req, res) => {
 });
 
 app.post("/SignUp", (req, res) => {
-  auth.sign_up(req.body).then((user) => {
+  auth.sign_up(req.body)
+  .then((user) => {
     req.session.user = user;
+    res.send(user)
+  })
+  .catch((err) => {
+    console.log(err)
+    res.status(401).send(err)
   })
 })
 
-server.listen(5000, () => console.log("backend online at 8080"));
+app.get("/test", (req, res) => {
+  res.send('Success');
+});
+
+server.listen(5000, () => console.log("backend online at 8100"));
