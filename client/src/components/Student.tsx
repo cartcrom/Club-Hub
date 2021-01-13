6e205c78bbe79dc81af83be7561dfb60fb43ff71
Vance Winstead <vance.winstead@gmail.com>
2021-01-13 11:53:51 -0800

    Updated frontend Student structure

--- client/src/components/Student.tsx
+++ client/src/components/Student.tsx
@@ -5,18 +5,28 @@ export default class Student {
   school: string;
   email: string;
   interests: Array<string>;
-  clubs: Array<string>;      // Stores ID's of clubs this student joined
+  major?: string;
+  collegeOf?: string;
+  favoriteClubType?: string;
+  joined_clubs: Array<string>;      // Stores ID's of clubs this student joined
   lead_clubs: Array<string>; // Stores ID's of clubs this student leads
 
-  constructor(fn : string, ln: string, id: number, school: string, email: string, interests: Array<string>, clubs: Array<string>, lead_clubs: Array<string>) {
+  constructor(fn : string, ln: string, id: number, school: string, email: string, interests: Array<string>, clubs: Array<string>, lead_clubs: Array<string>, major?: string, collegeOf?: string, favoriteClubType?: string) {
     this.fn = fn;
     this.ln = ln;
     this.id = id;
     this.school = school;
     this.email = email;
     this.interests = interests;
-    this.clubs = clubs;
+    this.joined_clubs = clubs;
     this.lead_clubs = lead_clubs;
+
+    if (major)
+      this.major = major
+    if (collegeOf)
+      this.collegeOf = collegeOf
+    if (favoriteClubType)
+      this.favoriteClubType = favoriteClubType
   }
 
   // Method
