98bd6d57a1fe657cffabd842d84c055ee2aa1c08
Jayant Devkar <55962509+JayantDevkar@users.noreply.github.com>
2021-01-26 01:03:02 +0530

    //cors bug

--- server/server.js
+++ server/server.js
@@ -32,6 +32,7 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
+app.use(cors())
 app.use(cookieParser("djaJK&(4kaUjfkbSU872dD3"));
 app.use(
   session({
