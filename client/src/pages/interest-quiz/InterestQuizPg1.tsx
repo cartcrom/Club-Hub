7d76fc7bba0b0105aa96910ae291afdb4e10372d
Vance Winstead <vance.winstead@gmail.com>
2021-02-08 11:43:34 -0800

    Added Linter

--- client/src/pages/interest-quiz/InterestQuizPg1.tsx
+++ client/src/pages/interest-quiz/InterestQuizPg1.tsx
@@ -1,62 +1,60 @@
-import React, { useState } from 'react';
-
-import { IonContent, IonLabel, IonSelect, IonPage, IonProgressBar, IonFooter, IonItem, IonIcon,IonInput, IonHeader, IonToolbar, IonButton, IonSelectOption} from '@ionic/react';
-import './InterestQuiz.css';
-import InterestQuiz from './InterestQuiz';
+import React, { ReactElement, useState } from "react";
+import { IonContent, IonLabel, IonSelect, IonPage, IonProgressBar, IonFooter, IonItem, IonSelectOption} from "@ionic/react";
+import "./InterestQuiz.css";
 
 interface InterestQuizProps {
-    nextPage: Function
-    updateSchoolInfo: Function
+    nextPage(): void;
+    updateSchoolInfo(s:string, c:string, m:string): void;
 }
 
 const addOptions = (fields: Array<string>) => {
-    let tags: JSX.Element[] = []
-    fields.forEach(optionName => {
-        tags.push(<IonSelectOption key={optionName} value={optionName}>{optionName}</IonSelectOption>)
-    });
-    return tags
+	const tags: JSX.Element[] = [];
+	fields.forEach(optionName => {
+		tags.push(<IonSelectOption key={optionName} value={optionName}>{optionName}</IonSelectOption>);
+	});
+	return tags;
 
-}
+};
 
-const InterestQuizPg1 = (props: InterestQuizProps) => {
-    const [schoolname, setSchool] =  useState<string>()
-    const [college, setCollege] =  useState<string>()
-    const [major, setMajor] =  useState<string>()
-    let schools = ["Cal Poly SLO", "Cal Poly Pomona", "Davis"]
-    let colleges = ["Engineering", "Science and Math", "Liberal Arts"]
-    let majors = ["Software Engineering", "Computer Science", "Architectural Engineering"]
+const InterestQuizPg1 = (props: InterestQuizProps) : ReactElement => {
+	const [schoolname, setSchool] =  useState<string>("Undefined");
+	const [college, setCollege] =  useState<string>("Undefined");
+	const [major, setMajor] =  useState<string>("Undefined");
+	const schools = ["Cal Poly SLO", "Cal Poly Pomona", "Davis"];
+	const colleges = ["Engineering", "Science and Math", "Liberal Arts"];
+	const majors = ["Software Engineering", "Computer Science", "Architectural Engineering"];
 
-    return (
-        <IonPage>
-            <IonContent fullscreen className="quiz-gradient" >
-                <IonProgressBar className="progress-bar" color="secondary" value={0.1}></IonProgressBar>
-                <h2 id="quiz-header">School Information </h2>
-                <form>
-                    <IonItem className="drop-down-button">
-                        <IonSelect value={schoolname} placeholder="School Name" className="center-elements" onIonChange={e => setSchool(e.detail.value)}> 
-                        {addOptions(schools)}
-                        </IonSelect>
-                    </IonItem>
-                    <IonItem className="drop-down-button">
-                        <IonSelect value={college} placeholder="College" className="center-elements" onIonChange={e => setCollege(e.detail.value)}> 
-                            {addOptions(colleges)}
-                        </IonSelect>
-                    </IonItem>
-                    <IonItem className="drop-down-button">
-                        <IonSelect value={major} placeholder="Major" className="center-elements" onIonChange={e => setMajor(e.detail.value)}> 
-                            {addOptions(majors)}
-                        </IonSelect>
-                    </IonItem>
-                </form>
-            </IonContent>
+	return (
+		<IonPage>
+			<IonContent fullscreen className="quiz-gradient" >
+				<IonProgressBar className="progress-bar" color="secondary" value={0.1}></IonProgressBar>
+				<h2 id="quiz-header">School Information </h2>
+				<form>
+					<IonItem className="drop-down-button">
+						<IonSelect value={schoolname} placeholder="School Name" className="center-elements" onIonChange={e => setSchool(e.detail.value)}> 
+							{addOptions(schools)}
+						</IonSelect>
+					</IonItem>
+					<IonItem className="drop-down-button">
+						<IonSelect value={college} placeholder="College" className="center-elements" onIonChange={e => setCollege(e.detail.value)}> 
+							{addOptions(colleges)}
+						</IonSelect>
+					</IonItem>
+					<IonItem className="drop-down-button">
+						<IonSelect value={major} placeholder="Major" className="center-elements" onIonChange={e => setMajor(e.detail.value)}> 
+							{addOptions(majors)}
+						</IonSelect>
+					</IonItem>
+				</form>
+			</IonContent>
             
-            <IonFooter className="transparent">
-                <IonItem button detail={false} className="white-outline-button" onClick={() => {props.updateSchoolInfo(schoolname, college, major); props.nextPage()}}>
-                    <IonLabel>next</IonLabel>
-                </IonItem>
-            </IonFooter>
-        </IonPage>
-    );
-  };
+			<IonFooter className="transparent">
+				<IonItem button detail={false} className="white-outline-button" onClick={() => {props.updateSchoolInfo(schoolname, college, major); props.nextPage();}}>
+					<IonLabel>next</IonLabel>
+				</IonItem>
+			</IonFooter>
+		</IonPage>
+	);
+};
   
-  export default InterestQuizPg1;
+export default InterestQuizPg1;
