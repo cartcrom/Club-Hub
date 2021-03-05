a2a5ab8905317a223e44360879f9a04e0cfc8023
Katie Seidl <kseidl@calpoly.edu>
2021-03-04 17:03:21 -0800

    fixed the match function in the datalist

--- client/src/pages/interest-quiz/DataList.js
+++ client/src/pages/interest-quiz/DataList.js
@@ -7,10 +7,13 @@ export default class DataList extends React.Component {
     constructor(props) {
         super(props);
     }
+    match = (currentInput, item) =>
+        item.label.toLowerCase().includes(currentInput.toLowerCase());
 
     render() {
         return (
             <DataListInput
+            match = {this.match}
             inputClassName="datalist-input"
             placeholder={this.props.placeholder}
             items={this.props.items}
