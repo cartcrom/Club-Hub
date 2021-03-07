2dba4c46df7b598d30f3e61bd584faa76dc721cc
Vance Winstead <vance.winstead@gmail.com>
2021-03-07 13:24:30 -0800

    Removed Code Duplication in InterestQuiz

---
+++ client/src/pages/interest-quiz/InterestQuizCheckboxTemplate.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { IonContent, IonProgressBar, IonPage, IonList, IonLabel, IonItem, IonCheckbox } from "@ionic/react";
+import "./InterestQuiz.css";
+
+
+interface InterestQuizProps {
+  nextPage: Function
+  addInterest: Function
+  checkboxes: {title: string, val: string}[]
+}
+
+const InterestQuizCheckboxTemplate = (props: InterestQuizProps) => {
+
+  return (
+    <IonPage>
+      <IonContent fullscreen className="quiz-gradient" >
+        <IonProgressBar className="progress-bar" color="secondary" value={0.3}></IonProgressBar>
+        <h2 id="quiz-header">I am Interested In:</h2>
+        <IonList className="transparent">
+          {props.checkboxes.map(({ title, val }, i) => (
+            <IonItem className="checkbox-box" key={i}>
+              <IonLabel>{title}</IonLabel>
+              <IonCheckbox className="checkbox" slot="start" value={val} onIonChange={e => props.addInterest(e.detail.value, e.detail.checked)} />
+            </IonItem>
+          ))}
+        </IonList>
+
+        <IonItem button detail={false} className="white-outline-button" onClick={() => props.nextPage()}>
+          <IonLabel>next</IonLabel>
+        </IonItem>
+      </IonContent>
+
+    </IonPage>
+  );
+};
+
+export default InterestQuizCheckboxTemplate;
--- client/src/pages/interest-quiz/InterestQuizPg2.tsx
+++ client/src/pages/interest-quiz/InterestQuizPg2.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { IonContent, IonProgressBar, IonPage, IonList, IonLabel, IonItem, IonCheckbox } from "@ionic/react";
 import "./InterestQuiz.css";
+import InterestQuizCheckboxTemplate from "./InterestQuizCheckboxTemplate";
 
 
 interface InterestQuizProps {
@@ -19,25 +19,7 @@ const InterestQuizPg2 = (props: InterestQuizProps) => {
 
 
   return (
-    <IonPage>
-      <IonContent fullscreen className="quiz-gradient" >
-        <IonProgressBar className="progress-bar" color="secondary" value={0.3}></IonProgressBar>
-        <h2 id="quiz-header">I am Interested In:</h2>
-        <IonList className="transparent">
-          {checkboxes.map(({ title, val }, i) => (
-            <IonItem className="checkbox-box" key={i}>
-              <IonLabel>{title}</IonLabel>
-              <IonCheckbox className="checkbox" slot="start" value={val} onIonChange={e => props.addInterest(e.detail.value, e.detail.checked)} />
-            </IonItem>
-          ))}
-        </IonList>
-
-        <IonItem button detail={false} className="white-outline-button" onClick={() => props.nextPage()}>
-          <IonLabel>next</IonLabel>
-        </IonItem>
-      </IonContent>
-
-    </IonPage>
+    <InterestQuizCheckboxTemplate {...props} checkboxes={checkboxes} />
   );
 };
 
