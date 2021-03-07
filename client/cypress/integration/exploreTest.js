e74e2f90a29fd201b47b98558e09434fd4b7ccb4
Katie Seidl <kseidl@calpoly.edu>
2021-03-07 15:08:42 -0800

    refixed explore tests structure

--- client/cypress/integration/exploreTest.js
+++ client/cypress/integration/exploreTest.js
@@ -1,4 +1,4 @@
-describe('Valid Explore Test', () => {
+describe('Explore Tests', () => {
   it('Tests finding an existing club', () => {
     cy.visit('http://localhost:8100/login')
     cy.contains('I have an account').click()
@@ -9,6 +9,16 @@ describe('Valid Explore Test', () => {
     cy.get('ion-searchbar').type('Ice')
     cy.contains('Ice Cream Club').click()
     cy.url().should('include', '/60088732d958ac35d881b6c9')
+  })
+  it('Tests finding a nonexistant club', () => {
+    cy.visit('http://localhost:8100/login')
+    cy.contains('I have an account').click()
+    cy.get('ion-input').eq(0).type('maxkennedy@school.edu')
+    cy.get('ion-input').eq(1).type('1234')
+    cy.contains('Log In').click()
+    cy.contains('EXPLORE').click()
+    cy.get('ion-searchbar').type('Nonexistant')
+    cy.contains('Nonexistant').should('not.exist')
   })
 })
 
