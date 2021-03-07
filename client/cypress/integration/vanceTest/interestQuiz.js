5b5655e9354cc33bba09e7d03cd8452f680ac7ce
Katie Seidl <kseidl@calpoly.edu>
2021-03-07 14:16:42 -0800

    working interest quiz acceptance test

--- client/cypress/integration/vanceTest/interestQuiz.js
+++ client/cypress/integration/vanceTest/interestQuiz.js
@@ -1,21 +1,12 @@
-/// <reference types="cypress" />
-context("Home", () => {
-  beforeEach(() => {
-    cy.visit("http://localhost:8100/signin");
-  });
-});
-it("test signin", () => {
-  //https://on.cypres.io/go
-
-  cy.location().should((Home) => {
-    expect(Home.hash).to.be.empty;
-    expect(Home.origin).to.eq("http://localhost:8100");
-    expect(Home.pathname).to.eq("/signin");
-  });
-  cy.get("ion-input").eq(0).type("maxkennedy@schoool.edu");
-  cy.get("ion-input").eq(1).type("1234");
-  cy.contains("Log In").click();
-  cy.url().should("include", "/interestQuiz");
-  cy.contains("let's go!").click();
-  cy.url().should("eq", "http://localhost:8100/interestQuiz");
-});
+describe('Vance\'s login test', () => {
+  it('Tests loging in', () => {
+    cy.visit('http://localhost:8100/login')
+    cy.contains('I have an account').click()
+    cy.get('ion-input').eq(0).type('maxkennedy@school.edu')
+    cy.get('ion-input').eq(1).type('1234')
+    cy.contains('Log In').click()
+    cy.url().should('include', '/interestQuiz')
+    cy.contains("let's go!").click();
+    cy.url().should("eq", "http://localhost:8100/login#/interestQuiz");
+  })
+})
