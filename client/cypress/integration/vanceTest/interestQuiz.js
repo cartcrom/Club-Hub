f35e14b8d7ccee000e5d6ded7bbe0ff2f903fad1
Katie Seidl <kseidl@calpoly.edu>
2021-03-07 14:58:45 -0800

    fixed the first interest quiz test to take the whole test and go to the explore page

--- client/cypress/integration/vanceTest/interestQuiz.js
+++ client/cypress/integration/vanceTest/interestQuiz.js
@@ -1,5 +1,5 @@
-describe('Vance\'s login test', () => {
-  it('Tests loging in', () => {
+describe('Valid InterestQuiz Test', () => {
+  it('Tests loging in, taking the interest quiz, and that the explore pages represents your interests', () => {
     cy.visit('http://localhost:8100/login')
     cy.contains('I have an account').click()
     cy.get('ion-input').eq(0).type('maxkennedy@school.edu')
@@ -8,5 +8,27 @@ describe('Vance\'s login test', () => {
     cy.url().should('include', '/interestQuiz')
     cy.contains("let's go!").click();
     cy.url().should("eq", "http://localhost:8100/login#/interestQuiz");
+    //fill out major information
+    cy.get("input").eq(0).type("Engineering\n");
+    cy.get("input").eq(1).type("Software Engineering\n");
+    cy.contains("next").click();
+    //fill out interests
+    cy.get("ion-checkbox").eq(1).click()
+    cy.get("ion-checkbox").eq(4).click()
+    cy.get("ion-checkbox").eq(5).click()
+    cy.contains("next").click();
+    cy.get("ion-checkbox").eq(0).click()
+    cy.get("ion-checkbox").eq(2).click()
+    cy.get("ion-checkbox").eq(4).click()
+    cy.get("ion-checkbox").eq(5).click()
+    cy.contains("next").click();
+    cy.get("ion-checkbox").eq(0).click()
+    cy.get("ion-checkbox").eq(1).click()
+    cy.get("ion-checkbox").eq(2).click()
+    cy.get("ion-checkbox").eq(3).click()
+    cy.contains("next").click();
+    //check explore tags represent that
+    cy.contains('EXPLORE').click()
+
   })
 })
